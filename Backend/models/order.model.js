b493ad1b14561e43f0c69373623e0b8f71acc972
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-11-29 11:35:41 -0800

    Creating Order Model

---
+++ Backend/models/order.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import User from "./user.model";
+
+const OrderSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: String,
+        required: true,
+    },
+    products: {
+        type: Array,
+        required: true,
+    },
+    total: {
+        type: Number,
+        required: true,
+    },
+    address: {
+        type: String,
+        
+    },
+    phone: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    status: {
+        type: Number,
+        default: 0,
+    },
+}, { timestamps: true })
