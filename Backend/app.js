dbc9b7afefe12dc47a90a30d6f83040c25528e89
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-11-29 13:22:26 -0800

    Generate Token and store in cookies:

--- Backend/app.js
+++ Backend/app.js
@@ -2,11 +2,15 @@
 import express from 'express';
 import cors from 'cors';
 import { notFound, errorHandler } from './middleware/errorHandler.middleware.js';
+import cookieParser from 'cookie-parser';
 const app = express();
 
 
 //json body
 app.use(express.json());
+
+//cookie parser
+app.use(cookieParser());
 
 //cors
 app.use(cors());
