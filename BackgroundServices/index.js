089ec6acc4d9d94e1cdd32cd7f9b21765242e085
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-12-01 06:47:41 -0800

    Introduction to Background services. Scheduling Task using node crone.

---
+++ BackgroundServices/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cron from "node-cron";
+import connectDB from "./utils/db.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+//SCHEDULED BACKGROUND SERVICES CAN BE ADDED HERE
+//if you want to learn how to schedule tasks i recommenend crontab guru
+const services = () => {
+    cron.schedule("* * * * * *", () => {
+        console.log("Background service running every second");
+        // Add your background task logic here
+    });
+};
+services();
+
+app.listen(PORT, () => {
+    console.log("Background Services is running on port", PORT);
+    connectDB();
+});
