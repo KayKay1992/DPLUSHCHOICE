a56d55baf69083c4f9a90c1b214be989cd971149
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-12-02 21:13:25 -0800

    Creating Multipage react application using react using react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,45 @@
-
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
+import MyAccount from "./pages/MyAccount";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-  
+  const Layout = () => {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  };
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/cart",
+          element: <Cart />,
+        },
+        {
+          path: "/myaccount",
+          element: <MyAccount />,
+        },
+      ],
+    },
+  ]);
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
