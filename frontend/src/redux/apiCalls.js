dd47cb64e6371e29dee78d8262e8eb628bcaa841
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-12-09 22:25:34 -0800

    Redux Store Configuration - Complete State Management Setup

---
+++ frontend/src/redux/apiCalls.js
@@ -0,0 +1,12 @@
+import { userRequest } from "../requestMethods";
+import { loginStart } from "./userRedux";
+
+export const login = async (dispatch, user) => {
+  dispatch(loginStart());
+  try {
+    const res = await userRequest.post("/auth/login/", user);
+    dispatch(loginSuccess(res.data));
+  } catch (error) {
+    dispatch(loginFailure());
+  }
+};
---
+++ frontend/src/redux/store.js
@@ -0,0 +1,32 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { FLUSH, PAUSE, PERSIST, persistReducer, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import cartReducer from "./cartRedux";
+import userReducer from "./userRedux";
+import persistStore from "redux-persist/es/persistStore";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+//   whitelist: ["cart", "user"],
+};
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export let persistor = persistStore(store);
